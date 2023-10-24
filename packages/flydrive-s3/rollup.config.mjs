1aef8a55b0ef3851263463255ece85867b51616a
Akhmad Salafudin <axmad386@gmail.com>
2023-10-24 11:34:35 +0700

    chore(flydrive-s3): support both cjs and esm

--- packages/flydrive-s3/rollup.config.mjs
+++ packages/flydrive-s3/rollup.config.mjs
@@ -2,7 +2,13 @@ import { bundleTs } from "@lunoxjs/build";
 import del from "rollup-plugin-delete";
 export default [
   ...bundleTs(["src/index.ts"], {
-    beforeBuild: [del({ targets: "dist/*" })],
+    beforeBuild: [del({ targets: "dist/esm/*" })],
     declaration: process.env.NODE_ENV == "production",
+    outputDir: "dist/esm",
+  }),
+  ...bundleTs(["src/index.ts"], {
+    format: "cjs",
+    beforeBuild: [del({ targets: "dist/cjs/*" })],
+    outputDir: "dist/cjs",
   }),
 ];
